81d12b781cddb28bc7c7812b81b5790dc0c77fd3
braulo <breuer.paulo@googlemail.com>
2022-06-20 17:06:47 +0200

    redirect when 404

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
+import App from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,5 +15,17 @@ function MyApp({ Component, pageProps }: AppProps) {
     </ThemeProvider>
   );
 }
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const appProps = await App.getInitialProps(context);
+
+  if (context.ctx.res?.statusCode === 404) {
+    context.ctx.res.writeHead(302, { Location: "/" });
+    context.ctx.res.end();
+    return;
+  }
+
+  return { ...appProps };
+};
 
 export default MyApp;
