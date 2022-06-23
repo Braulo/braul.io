1ef8e294a5b0c33ddd1c791d0470a929a6d9d3f2
braulo <breuer.paulo@googlemail.com>
2022-06-23 17:36:02 +0200

    stuff

---
+++ types/PrivateProject.ts
@@ -0,0 +1,7 @@
+export interface PrivateProject {
+  name: string;
+  description: string;
+  website?: string | null;
+  status: string;
+  techstack: string;
+}
---
+++ utils/private-projects.ts
@@ -0,0 +1,42 @@
+import { PrivateProject } from "../types/PrivateProject";
+
+export const privateProjects: PrivateProject[] = [
+  {
+    name: "Dango",
+    description: "dango-description",
+    website: null,
+    status: "dango-status",
+    techstack: "Angular, Express.js, SQL",
+  },
+  {
+    name: "BetterChat",
+    description: "betterchat-description",
+    website: null,
+    status: "betterchat-status",
+    techstack: "React, Next.js, Express.js, SQL",
+  },
+  {
+    name: "Battle Royale Chest Opener",
+    description: "br-description",
+    website:
+      "https://apkpure.com/battle-royale-chest-opener/com.cetapp.opener.battleroyal.chest",
+    status: "br-status",
+    techstack: "Java, Firebase, SQL",
+  },
+  {
+    name: "Battle Royale Stickers for Whatsapp",
+    description: "stickers-description",
+    website:
+      "https://apkpure.com/battle-royale-stickers/com.cetapp.sticker.battleroyale",
+    status: "stickers-status",
+    techstack: "Java, Firebase",
+  },
+  {
+    name: "TaDa Daily Tasks",
+    description: "tada-description",
+    website:
+      "https://play.google.com/store/apps/details?id=com.cetaplays.cetanet.tadaapp",
+    status: "tada-status",
+    techstack: "Java, Firebase, SQL",
+  },
+];
